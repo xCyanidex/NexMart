e9e39334498dd906d1ebaae331615ebcfdfbee78
Duraiz Haider <86344507+xCyanidex@users.noreply.github.com>
2024-01-16 00:08:50 +0500

    first commit

---
+++ frontend/src/main.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+createBrowserRouter,
+createRoutesFromElements,
+Route,
+RouterProvider,
+} from 'react-router-dom';
+import "./assets/styles/bootstrap.custom.css";
+import App from './App.jsx'
+import './index.css'
+import "./assets/styles/index.css"
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from "./screens/ProductScreen";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
