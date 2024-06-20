1c85701dcb77a95297fdb466d17f1ec2997d2dca
xCyanidex <haiderduraiz@gmail.com>
2024-06-20 09:35:27 +0500

    changed some paths

--- backend/server.js
+++ backend/server.js
@@ -36,12 +36,13 @@ const __dirname=path.resolve();
 app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
 
 if(process.env.NODE_ENV==='production'){
-
-    app.use(express.static(path.join(__dirname,'frontend/build')));
-
+    
+    app.use(express.static(path.join(__dirname,'frontend/dist')));
+    
     app.get('*',(req,res)=>
-    res.sendFile(path.resolve(__dirname,'frontend','build','index.html ')))
+        res.sendFile(path.resolve(__dirname,'frontend','dist','index.html ')))
 }else {
+    console.log("Reached ehre")
     app.get('/', (req, res) => {
         res.send('Api is running');
     });
