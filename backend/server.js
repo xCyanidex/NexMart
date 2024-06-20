783b9173d7395c494b40c9abb51ee0ce99b293bf
xCyanidex <haiderduraiz@gmail.com>
2024-06-20 06:32:27 +0500

    added paypal fixes

--- backend/server.js
+++ backend/server.js
@@ -24,9 +24,6 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
 
-app.get('/',(req,res)=>{
-res.send('Api is running');
-});
 
 app.use('/api/products',productRoutes);
 app.use('/api/users', userRoutes);
@@ -37,6 +34,19 @@ app.get('/api/config/paypal',(req,res)=> res.send({clientId:process.env.PAYPAL_C
 
 const __dirname=path.resolve();
 app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
+
+if(process.env.NODE_ENV==='producttion'){
+
+    app.use(express.static(path.join(__dirname,'frontend/build')));
+
+    app.get('*',(req,res)=>
+    res.sendFile(path.resolve(__dirname,'frontend','build','index.html ')))
+}else {
+    app.get('/', (req, res) => {
+        res.send('Api is running');
+    });
+
+}
 
 app.use(notFound);
 app.use(errorHandler);
